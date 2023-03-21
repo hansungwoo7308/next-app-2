d506b5dc872f1824ccb3860d9adc03fc085cf342
hansungwoo7308 <hansungwoo7308@gmail.com>
2023-03-21 22:25:43 +0900

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,29 +1,37 @@
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 const Nav = () => {
   // get and set
   const router = useRouter();
+  const focusRef = useRef();
+  const homeRef = useRef();
+  const blogsRef = useRef();
+  const postsRef = useRef();
   const [currentPage, setCurrentPage] = useState("home");
   const home = currentPage === "home" ? "selectedMenu" : "unselectedMenu";
   const blogs = currentPage === "blogs" ? "selectedMenu" : "unselectedMenu";
-
-  // set the something
-  // const [focus, setFocus] = useState()
-  const focusRef = useRef();
-  const listRef = useRef();
+  const posts = currentPage === "posts" ? "selectedMenu" : "unselectedMenu";
 
   // process
   const handleChange = () => {
+    focusRef.current.style.width =
+      focusRef.current.nextElementSibling.getBoundingClientRect().width + "px";
+    const offsetHome = homeRef.current.offsetLeft + "px";
+    const offsetBlogs = blogsRef.current.offsetLeft + "px";
+    const offsetPosts = postsRef.current.offsetLeft + "px";
+
     if (router.pathname === "/") {
       setCurrentPage("home");
-      // focusRef.current.style = "border-bottom: 3px solid coral";
+      focusRef.current.style.left = offsetHome;
     } else if (router.pathname === "/blogs") {
       setCurrentPage("blogs");
-    } else if (router.pathname === "/auth/signin") {
-      setCurrentPage("signin");
+      focusRef.current.style.left = offsetBlogs;
+    } else if (router.pathname === "/posts") {
+      setCurrentPage("posts");
+      focusRef.current.style.left = offsetPosts;
     } else {
       setCurrentPage("");
     }
@@ -32,47 +40,31 @@ const Nav = () => {
   const handleFocus = (e) => {
     const newLeft = e.target.offsetLeft;
     const newWidth = e.target.offsetWidth;
-    console.log("newLeft : ", newLeft);
-    console.log("newWidth : ", newWidth);
-    // focusRef.current.style = 'outline: 3px solid coral'
     focusRef.current.style.left = newLeft + "px";
     focusRef.current.style.width = newWidth + "px";
+    // console.log("newLeft : ", newLeft);
+    // console.log("newWidth : ", newWidth);
   };
 
   useEffect(() => {
     handleChange();
   });
 
-  // set the initial reference settings
-  useEffect(() => {
-    focusRef.current = listRef.current.firstChild;
-    focusRef.current.style.width =
-      focusRef.current.nextElementSibling.getBoundingClientRect().width;
-    listRef.current = listRef.current.childNodes;
-  }, []);
-
   return (
     <nav>
-      <ul ref={listRef}>
-        <li className="focus"></li>
-        <li
-          className={home}
-          onClick={(e) => {
-            handleFocus(e);
-          }}
-
-          // ref={currentPage === "home" ? focusRef : null}
-        >
+      <ul>
+        <li className="focus" ref={focusRef}></li>
+        <li className={home} ref={homeRef} onClick={(e) => handleFocus(e)}>
           <Link href={"/"}>HOME</Link>
         </li>
-        <li className={blogs} onClick={(e) => handleFocus(e)}>
+        <li className={blogs} ref={blogsRef} onClick={(e) => handleFocus(e)}>
           <Link href={"/blogs"}>BLOGS</Link>
+        </li>
+        <li className={posts} ref={postsRef} onClick={(e) => handleFocus(e)}>
+          <Link href={"/posts"}>POSTS</Link>
         </li>
         {/* <li>
           <Link href={"/about"}>ABOUT</Link>
-        </li> */}
-        {/* <li>
-          <Link href={"/posts"}>POSTS</Link>
         </li> */}
 
         {/* <li>

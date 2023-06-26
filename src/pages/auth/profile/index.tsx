014fe694cb85d0034ea2ff9c3f1df879e1146d87
hansungwoo7308 <hansungwoo7308@gmail.com>
2023-06-26 11:37:18 +0900

    update

--- src/pages/auth/profile/index.tsx
+++ src/pages/auth/profile/index.tsx
@@ -39,26 +39,27 @@ import Image from "next/image";
 // }
 export default function Page() {
   const { auth }: any = useSelector((store) => store);
-  const { accessToken } = auth;
-  const [users, setUsers]: any = useState();
-  const router = useRouter();
-  const getData = async () => {
-    try {
-      const response = await axios({
-        method: "get",
-        url: `/api/users?mode=${auth?.mode}`,
-        headers: {
-          Authorization: `Bearer ${accessToken ? accessToken : ""}`,
-        },
-      });
-      logResponse(response);
-      setUsers(response.data.users);
-    } catch (error) {
-      logError(error);
-      // console.log(error);
-      // await refreshAuth();
-    }
-  };
+  const [mode, setMode]: any = useState("view");
+  const initialState = {};
+  //   const { accessToken } = auth;
+  //   const [users, setUsers]: any = useState();
+  //   const getData = async () => {
+  //     try {
+  //       const response = await axios({
+  //         method: "get",
+  //         url: `/api/users?mode=${auth?.mode}`,
+  //         headers: {
+  //           Authorization: `Bearer ${accessToken ? accessToken : ""}`,
+  //         },
+  //       });
+  //       logResponse(response);
+  //       setUsers(response.data.users);
+  //     } catch (error) {
+  //       logError(error);
+  //       // console.log(error);
+  //       // await refreshAuth();
+  //     }
+  //   };
   // useEffect(() => {
   //   getData();
   // }, [auth]);
@@ -70,13 +71,42 @@ export default function Page() {
       </Head>
       <Main>
         <section>
-          <div>
-            <h1>Profile</h1>
-            <h3>Username : {auth.username}</h3>
-            <h3>Role : {auth.role}</h3>
-          </div>
-          <div>
-            {auth.image && <Image src={auth.image} alt={"auth.image"} width={200} height={200} />}
+          <div className="profile">
+            <div className="image">
+              {auth.image && <Image src={auth.image} alt={"auth.image"} width={200} height={200} />}
+            </div>
+            <div className="description">
+              {mode === "view" && (
+                <>
+                  <div>
+                    <h1>Profile</h1>
+                    <div>
+                      <h3>Name : {auth.username}</h3>
+                    </div>
+                    <div>
+                      <h3>Role : {auth.role}</h3>
+                    </div>
+                  </div>
+                  <button onClick={() => setMode("edit")}>Edit</button>
+                </>
+              )}
+              {mode === "edit" && (
+                <>
+                  <div>
+                    <h1>Profile</h1>
+                    <div>
+                      <span>Name :</span>
+                      <input type="text" defaultValue={auth.username} />
+                    </div>
+                    <div>
+                      <span>Image :</span>
+                      <input type="text" defaultValue={auth.username} />
+                    </div>
+                  </div>
+                  <button onClick={() => setMode("view")}>Update</button>
+                </>
+              )}
+            </div>
           </div>
         </section>
       </Main>
@@ -85,12 +115,38 @@ export default function Page() {
 }
 const Main = styled(PublicMain)`
   > section {
-    > div {
-      width: 80%;
+    .profile {
       height: 70vh;
+      display: flex;
+      justify-content: space-between;
       padding: 20px;
-      > h1 {
-        margin-bottom: 20px;
+      > div {
+        border: 2px solid;
+      }
+      .description {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding: 1rem;
+        div {
+          /* * {
+            border: 2px solid;
+          } */
+          h1 {
+            margin-bottom: 20px;
+          }
+        }
+        button {
+          width: 5rem;
+          align-self: flex-end;
+        }
+      }
+      .image {
+        width: 10rem;
+        img {
+          height: initial;
+        }
       }
     }
   }

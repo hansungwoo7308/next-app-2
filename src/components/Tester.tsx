bf322adfbd6f75d2637e6e10b399a88c60ed2c17
hansungwoo7308 <hansungwoo7308@gmail.com>
2023-09-12 10:32:31 +0900

    update

--- src/components/Tester.tsx
+++ src/components/Tester.tsx
@@ -1,37 +1,37 @@
 import styled from "styled-components";
-import { Variants, motion } from "framer-motion";
+import { Transition, Variants, motion } from "framer-motion";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
-const imageVariants: any = {
-  animate: { width: "100%" },
-};
-const transition = { duration: 2, ease: [0.6, 0.01, -0.05, 0.9] };
-const messageParentVariants: Variants = {
-  animate: {
-    transition: {
-      duration: 1,
-      delayChildren: 1,
-      staggerChildren: 0.1,
-    },
-  },
-};
-const messageChildrenVariants = {
-  initial: { y: 100, opacity: 0 },
-  animate: {
-    y: 0,
-    opacity: 1,
-    transition: { duration: 1 },
-  },
-};
+// const imageVariants: Variants = { animate: { width: "100%" } };
+// const imageTransition: Transition = { duration: 2, ease: [0.6, 0.01, -0.05, 0.9] };
+// const messageParentVariants: Variants = {
+//   animate: {
+//     transition: {
+//       duration: 1,
+//       delayChildren: 1,
+//       staggerChildren: 0.1,
+//     },
+//   },
+// };
+// const messageChildrenVariants: Variants = {
+//   initial: { y: 300, opacity: 0 },
+//   animate: {
+//     y: 0,
+//     opacity: 1,
+//     // transition: { duration: 1 },
+//     // transition: { duration: 1, ease: [0.6, 0.01, -0.05, 0.9] },
+//     transition: { duration: 1, ease: "easeOut" },
+//   },
+// };
 export default function Tester() {
   return (
     <Box>
       {/* image animation */}
       <motion.div
         className="something-outer"
-        variants={imageVariants}
+        variants={{ animate: { width: "100%" } }}
         animate="animate"
-        transition={transition}
+        transition={{ duration: 2, ease: [0.6, 0.01, -0.05, 0.9] }}
         layoutId="123"
       >
         <Link href={"/test"}>
@@ -41,13 +41,34 @@ export default function Tester() {
       {/* message animation */}
       <motion.div
         className="message"
-        variants={messageParentVariants}
+        variants={{
+          animate: {
+            transition: {
+              duration: 1,
+              delayChildren: 1,
+              staggerChildren: 0.1,
+            },
+          },
+        }}
         // endpoint의 애니메이션의 설정을 패런트에서 해준다.
         initial="initial"
         animate="animate"
       >
         {"something...".split("").map((v: any) => (
-          <motion.span variants={messageChildrenVariants}>{v}</motion.span>
+          <motion.span
+            variants={{
+              initial: { y: 300, opacity: 0 },
+              animate: {
+                y: 0,
+                opacity: 1,
+                // transition: { duration: 1 },
+                // transition: { duration: 1, ease: [0.6, 0.01, -0.05, 0.9] },
+                transition: { duration: 1, ease: "easeOut" },
+              },
+            }}
+          >
+            {v}
+          </motion.span>
         ))}
       </motion.div>
       {/* <motion.div className="test-image" whileHover={{ scale: 1.1 }}>
@@ -70,9 +91,12 @@ const Box = styled.div`
     overflow: hidden;
   }
   .message {
+    height: 100px;
+    /* border: 2px solid; */
     position: absolute;
     place-self: center;
     font-size: 70px;
+    overflow: hidden;
     span {
       display: inline-block;
     }
